00562c86e40b972cad0c7cf01a6e8019e2a45724
anibalUniversales <aemunozh@universales.com>
2023-08-03 15:20:57 -0600

    Constrains en los campos

--- Aprendiendo/ViewController.swift
+++ Aprendiendo/ViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //MARK: - Referencias UI
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func loginButton() {
+        //1. Obtener los valores de los textfield.
+        
+        let email = emailTextField.text
+        let password = passwordTextField.text
+        
+        if(email == "carlos@mejia.com" && password == "123"){
+            performSegue(withIdentifier: "home_segue", sender: nil)
+        }else{
+            print("Credenciales inválidas")
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
